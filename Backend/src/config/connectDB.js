1d4f2fe4a4eb07a39fa4dbb8adfd121fcb72b3ac
Tphuong00 <tphuong0210@gmail.com>
2024-12-28 02:39:26 +0700

    docker

--- Backend/src/config/connectDB.js
+++ Backend/src/config/connectDB.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, 'root', process.env.DB_PASSWORD,{
-    host: process.env.DB_HOST,
+    host: "host.docker.internal",
     dialect:'mysql',
     logging: false,
     dialectOptions: {
