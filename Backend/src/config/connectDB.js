c73727cd71a4879f539f4cc185c149f64f8524ac
Tphuong00 <tphuong0210@gmail.com>
2024-12-27 13:09:50 +0700

    noti

--- Backend/src/config/connectDB.js
+++ Backend/src/config/connectDB.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-    host: process.env.DB_HOST||'localhost' ,
+    host: process.env.DB_HOST,
     dialect:'mysql',
     logging: false,
     dialectOptions: {
