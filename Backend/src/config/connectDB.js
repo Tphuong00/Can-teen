11dd05b64042de82d51bf6777381ea237b9a8cfc
Tphuong00 <tphuong0210@gmail.com>
2024-12-28 20:33:00 +0700

    docker

--- Backend/src/config/connectDB.js
+++ Backend/src/config/connectDB.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, 'root', process.env.DB_PASSWORD,{
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     dialect:'mysql',
     logging: false,
