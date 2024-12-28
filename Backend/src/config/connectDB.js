814f230a66d97aee0b61161bf869812111363126
Tphuong00 <tphuong0210@gmail.com>
2024-12-28 22:19:23 +0700

    docker

--- Backend/src/config/connectDB.js
+++ Backend/src/config/connectDB.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     dialect:'mysql',
     logging: false,
+    port: process.env.PORT,
     dialectOptions: {
         connectTimeout: 60000,  // Tăng thời gian chờ kết nối lên 20 giây
     },
