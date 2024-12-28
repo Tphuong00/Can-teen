bcb82be81a32909948fc924284ca80de725fcb56
Tphuong00 <tphuong0210@gmail.com>
2024-12-29 03:15:37 +0700

    fix-https

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -79,7 +79,7 @@ app.use((req, res)=>{
 
 let PORT = process.env.PORT||8080;
 
-app.listen(port,'0.0.0.0', () =>{
+app.listen(PORT,'0.0.0.0', () =>{
     console.log("Backend is running on the port: " + PORT);
 })
 
