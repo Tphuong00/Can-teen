97871f65cee34f3094385d691c5dd1d6b134d543
Tphuong00 <tphuong0210@gmail.com>
2024-12-29 03:09:42 +0700

    fix-https

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -77,7 +77,7 @@ app.use((req, res)=>{
     return res.send('404 not found');
 })
 
-let port = process.env.PORT;
+let port = process.env.PORT||8080;
 
 app.listen(port,'0.0.0.0', () =>{
     console.log("Backend is running on the port: " + port);
