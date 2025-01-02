7a1ccedc80ca1bf061d0a0bf9973288eaf04960d
Tphuong00 <tphuong0210@gmail.com>
2025-01-02 11:47:35 +0700

    fix-bug-frontend-host

--- base-react-hook-tutorial-master/src/setup/axios.js
+++ base-react-hook-tutorial-master/src/setup/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // Set config defaults when creating the instance
 const instance = axios.create({
     baseURL:  process.env.REACT_APP_BACKEND_URL,
+    withCredentials: true,
   });
 
 instance.defaults.withCredentials = true;
