3fe37ab0ec78c50b34916c1bd407da18df19586d
Tphuong00 <tphuong0210@gmail.com>
2025-01-02 12:30:30 +0700

    fix-bug-frontend-host

--- base-react-hook-tutorial-master/src/setup/axios.js
+++ base-react-hook-tutorial-master/src/setup/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-    baseURL:  process.env.REACT_APP_BACKEND_URL,
+    baseURL:  "https://canteen-backend-production.up.railway.app",
     withCredentials: true,
   });
 
