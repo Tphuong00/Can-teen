07c43754e689ee878600d42dc0472288cd33bb49
Tphuong00 <tphuong0210@gmail.com>
2025-01-05 03:53:17 +0700

    fix-checkauth1

--- base-react-hook-tutorial-master/src/setup/axios.js
+++ base-react-hook-tutorial-master/src/setup/axios.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
-import axios from 'axios';
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjo2LCJmdWxsTmFtZSI6ImZpc2FpYm5jYmh4Y2dkZyIsImVtYWlsIjoicnViaW5nb2sxMEBnbWFpbC5jb20iLCJwaG9uZU51bWJlciI6IjA5ODY5NTc4OCIsImFkZHJlc3MiOiJxMSwgSENNIiwicm9sZUlEIjoiMSJ9LCJpYXQiOjE3MzUyODkxMjEsImV4cCI6MTczNzg4MTEyMX0.cRWsVXTBE6_Squp4O0r2_5LBbuoCJlgZ6ie2oTzR9rg'; // Lấy token từ cookie hoặc sessionStorage
+axios.get('https://can-teen.vercel.app/api/check-auth', {
+  headers: {
+    Authorization: `Bearer ${token}` // Đính kèm token vào Authorization header
+  }
+})
+  .then(response => console.log(response))
+  .catch(error => console.error(error));
+
 
 // Set config defaults when creating the instance
 const instance = axios.create({
