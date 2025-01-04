481d75ba7fc9ee7db61726454f5739ad20067001
Tphuong00 <tphuong0210@gmail.com>
2025-01-05 04:04:44 +0700

    fix-checkauth2

--- base-react-hook-tutorial-master/src/services/checkAuth.js
+++ base-react-hook-tutorial-master/src/services/checkAuth.js
@@ -1,7 +1,7 @@
 import axios from "../setup/axios";
 
 const checkAuth = () => {
-    return axios.get("/api/checkAuth");
+    return axios.get("/api/checkAuth", {withCredentials: true});
 }
 const logout = async () => {
     return await axios.get("/api/logout");
