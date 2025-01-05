b6503e6472be9905229584ba501df2f66b7d5d5c
Tphuong00 <tphuong0210@gmail.com>
2025-01-05 11:32:25 +0700

    fix-checkauth4

--- base-react-hook-tutorial-master/src/components/Home/MessengerChat.js
+++ base-react-hook-tutorial-master/src/components/Home/MessengerChat.js
@@ -3,41 +3,32 @@ import React, { useEffect } from 'react';
 const MessengerChat = () => {
   useEffect(() => {
     const loadFbSDK = () => {
-      // Kiểm tra xem SDK Facebook đã được tải chưa
-      if (window.FB) {
-        console.log("FB SDK loaded"); 
-        window.FB.XFBML.parse(); // Phân tích và render các thành phần Facebook (bao gồm Messenger Chat)
-      } else {
-        // Nếu chưa tải, thêm sự kiện để tải SDK
+      if (!window.FB) {
         const script = document.createElement('script');
         script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
         script.async = true;
+        script.defer = true;
         document.body.appendChild(script);
-            console.log("FB SDK not loaded yet");
         script.onload = () => {
-          // Sau khi tải xong SDK, phân tích lại
-          if (window.FB) { 
-            window.FB.XFBML.parse();
-          } 
+          window.FB?.XFBML.parse();
         };
       }
     };
-
-    loadFbSDK(); // Gọi hàm để tải SDK
-
-  }, []); 
+    loadFbSDK();
+  }, []);
 
   return (
-    <div>
+    <>
+      <div id="fb-root"></div>
       <div
         className="fb-customerchat"
         attribution="setup_tool"
-        page_id="61570959760002" // Thay thế bằng Page ID của bạn
+        page_id="YOUR_PAGE_ID" // Thay bằng Page ID của bạn
         theme_color="#0084ff"
         logged_in_greeting="Chào bạn, chúng tôi có thể giúp gì cho bạn?"
         logged_out_greeting="Chào bạn, hãy liên hệ với chúng tôi nhé!"
       ></div>
-    </div>
+    </>
   );
 };
 
